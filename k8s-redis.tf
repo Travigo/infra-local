1afb5b62f9c7b49b6ca7e8d0b9b68c7453b7249b
Aaron Claydon <claydon.aaron@gmail.com>
2022-04-14 17:51:20 +0100

    Create redis server

---
+++ k8s-redis.tf
@@ -0,0 +1,50 @@
+resource "kubernetes_namespace" "redis" {
+  metadata {
+    name        = "redis"
+    annotations = {}
+    labels      = {}
+  }
+}
+
+resource "random_password" "redis-password" {
+  length           = 64
+  special          = false
+}
+
+resource "kubernetes_secret" "redis-password" {
+  metadata {
+    name      = "redis-password"
+  }
+
+  data = {
+    "password" = random_password.redis-password.result
+  }
+
+  type = "kubernetes.io/secret"
+}
+
+resource "helm_release" "redis" {
+  name       = "redis"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "redis"
+
+  namespace = kubernetes_namespace.redis.metadata[0].name
+
+  set {
+    name  = "global.redis.password"
+    value = random_password.redis-password.result
+  }
+  set {
+    name = "master.persistence.enabled"
+    value = "false"
+  }
+  set {
+    name = "replica.persistence.enabled"
+    value = "false"
+  }
+  set {
+    name = "replica.replicaCount"
+    value = "1"
+  }
+}
